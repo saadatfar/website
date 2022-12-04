09fa1178b716aab07c247f1ee907e900d3dfd2de
Mohammad Hosein Saadatfar <mh.saadatfar@gmail.com>
2022-12-04 16:46:59 +0330

    Update profile picture.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mhsaadatfar.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
-  server: { port: 3001, host: true}
+  server: { port: 12345, host: true},
+  integrations: [
+    mdx(), sitemap(), tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
